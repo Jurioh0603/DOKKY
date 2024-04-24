d4083fe87da43a1f2eb5a3d5f74171b3476c5278
Jurioh0603 <julioh0603@gmail.com>
2024-04-24 14:15:36 +0900

    sort lunch

--- src/lunch/controller/ListLunchController.java
+++ src/lunch/controller/ListLunchController.java
@@ -16,18 +16,26 @@ public class ListLunchController implements CommandHandler {
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		String search = req.getParameter("search");
 		String pageNoVal = req.getParameter("pageNo");
+		String sort = req.getParameter("sort");
+		
 		int pageNo = 1;
 		if (pageNoVal != null) {
 			pageNo = Integer.parseInt(pageNoVal);
 		}
-
+		
+		if(sort == null) {
+			sort = "bno";
+		}
+		
 		LunchPage lunchPage = null;
 		if(search == null)
-			lunchPage = listService.getLunchPage(pageNo);
+			lunchPage = listService.getLunchPage(pageNo, sort);
 		else
-			lunchPage = listService.getSearchLunchPage(pageNo, search);
+			lunchPage = listService.getSearchLunchPage(pageNo, search, sort);
+		
 		req.setAttribute("lunchPage", lunchPage);
 		req.setAttribute("search", search);
+		req.setAttribute("sort", sort);
 		return "/view/board/lunch/lunchSelect.jsp";
 	}
 }
